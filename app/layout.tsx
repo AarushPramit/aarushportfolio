8fa4d7a85999ddacc2d9c8c1e05aa63af973cfba
Aarush Pramit <aarushpramit062005@gmail.com>
2026-09-06 00:50:25 +0530

    removed middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import Intro from "@/components/intro";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={dmSans.className}>
+        <Intro />
         {children}
       </body>
     </html>
